301dd9798d7888285bbc7e1958061e18734d27ef
Russell Pondivida <russellpondivida@Russells-MacBook-Pro.local>
2021-11-25 13:59:32 +0800

    added script8.js

---
+++ js/script8.js
@@ -0,0 +1,10 @@
+let c1 = {name: "cinema 1", location: "north edsa", capacity: 500};
+
+function show (movie){
+    c1 = 'cinema 1';
+
+}
+
+function bind(){
+    console.log("c1.show" + "is showing End Game");
+}
